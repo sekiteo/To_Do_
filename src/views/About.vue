84a00243b463eca05336fb5381fa0b8d8126df8f
Ditchphong <95395036+sekiteo@users.noreply.github.com>
2022-02-11 13:31:11 +0700

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <br>
+    <center>
+    <!--  <h1>This is an about page.</h1>  -->
+    <h1>DITCHAPHONG</h1>
+    <h3>6304101327</h3>
+    </center>
+    
+  </div>
+</template>
